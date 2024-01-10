eff3ae9331234f68a0a872b138b90059cc547d51
higgi270 <111325440+higgi270@users.noreply.github.com>
2024-01-10 15:20:34 -0600

    ok

---
+++ server/data/data.js
@@ -0,0 +1,28 @@
+export const kpis = [
+  {
+    _id: "GDP and Global Warming",
+    totalLoss: "-10.516",
+    yearlyData: [
+      {
+        year: "2027",
+        loss: "-1.124",
+      },
+      {
+        year: "2037",
+        loss: "-2.248",
+      },
+      {
+        year: "2047",
+        loss: "-3.402",
+      },
+      {
+        year: "2067",
+        loss: "-5.743",
+      },
+      {
+        year: "2100",
+        loss: "-19.516",
+      },
+    ],
+  },
+];
